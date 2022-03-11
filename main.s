4bda78d4a82a0e0a918aed21ea647ee7edeacc01
Psc19 <Psc19@skbl-422-004.ph.ic.ac.uk>
2022-03-11 11:49:05 +0000

    WORKING BUTTONS

--- main.s
+++ main.s
@@ -1,5 +1,14 @@
 	#include <xc.inc>
-
+psect	udata_acs   ; named variables in access ram
+b_hldr:	ds 1
+b1:	ds 1
+b2:	ds 1 
+b3:	ds 1 
+b4:	ds 1 
+b5:	ds 1
+b6:	ds 1 
+b7:	ds 1 
+b8:	ds 1 
 psect	code, abs
 	
 main:
@@ -8,17 +17,45 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
+        banksel PADCFG1
+        bsf REPU
+        clrf LATE
+        banksel	0 
+	movlw 0xff
+	movwf TRISE
+	movlw 0x00
+	movwf TRISD
+	movff PORTE, WREG 
+	movwf PORTD	    
+	
 loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
+    	comf PORTE ,W 
+	movwf b_hldr
+	movlw 00000001B
+	andwf b_hldr, W
+	movwf b1
+	movlw 00000010B
+	andwf b_hldr, W
+	movwf b2
+	movlw 00000100B
+	andwf b_hldr, W
+	movwf b3
+	movlw 00001000B
+	andwf b_hldr, W
+	movwf b4
+	movlw 00010000B
+	andwf b_hldr, W
+	movwf b5
+	movlw 00100000B
+	andwf b_hldr, W
+	movwf b6
+	movlw 01000000B
+	andwf b_hldr, W
+	movwf b7
+	movlw 10000000B
+	andwf b_hldr, W
+	movwf b8
+	movff b8,PORTD
 	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
 
 	end	main
